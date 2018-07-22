a79d91f5273f56cd91f643903377c5e3a27fbb76
Kambiz <k.saffarizadeh@gmail.com>
2018-07-21 23:37:44 -0400

    New about page

---
+++ app/src/main/java/com/helpaigrow/AboutActivity.java
@@ -0,0 +1,54 @@
+package com.helpaigrow;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.Objects;
+
+public class AboutActivity extends AppCompatActivity {
+    WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        setTitle("About");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        }
+
+        String aboutPageUrl = "http://amandabot.xyz/about/110";
+
+        try {
+            webView = findViewById(R.id.aboutWebView);
+            webView.setWebViewClient(new WebViewClient());
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webView.loadUrl(aboutPageUrl);
+        } catch(Exception e){
+            Intent launchBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse(aboutPageUrl));
+            startActivity(launchBrowser);
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Intent goBackIntent = new Intent(AboutActivity.this, WelcomeActivity.class);
+        goBackIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(goBackIntent);
+    }
+
+}
